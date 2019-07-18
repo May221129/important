8b9b5855cc7c3c03115bf2466cf0edef3dec5239
lailimei <2350948180@qq.com>
2019-07-18 15:09:53 +0800

    数组的反转

--- src/Algorithm/ArrayReversal.java
+++ src/Algorithm/ArrayReversal.java
@@ -4,8 +4,13 @@ import java.util.Stack;
 
 /**
  * 数组的反转，如：{1,2,3}反转为{3,2,1}.
- * 实现方式：1.栈：压栈、弹栈；  2.首尾交换，i++, j--
- * 注意点：边界问题。
+ * 	1. 实现方式：1.栈：压栈、弹栈；  2.首尾交换，i++, j--
+ * 	2. 两种实现方式的效率对比：
+ * 		（时间单位：毫秒）		栈			首尾交换
+ * 			10万					19			1
+ * 			100万				147			5
+ * 			1000万				3040		11
+ * 	3. 结论：“首尾交换”的方式来完成数组的反转，明显优于“栈”的方式。
  * 
  * @author May
  * 2019年7月16日
@@ -13,7 +18,7 @@ import java.util.Stack;
 public class ArrayReversal {
 	public static void main(String[] args) {
 		ArrayReversal test = new ArrayReversal();
-		int number = 10000 * 1;
+		int number = 10000 * 1000;
 		int[] array1 = new int[number];
 		int[] array2 = new int[number];
 		for(int i = 0; i < number; i++) {
@@ -21,11 +26,13 @@ public class ArrayReversal {
 			array2[i] = i;
 		}
 		
+//		test.printElement(array1);
+		
 		test.arrayReversal(array1);
-		test.printElement(array1);
+//		test.printElement(array1);
 		System.out.println("--------------------------");
 		test.arrayReversalByStack(array2);
-		test.printElement(array2);
+//		test.printElement(array2);
 	}
 	
 	/**
@@ -37,9 +44,14 @@ public class ArrayReversal {
 		for(Integer element : target) {
 			stack.push(element);
 		}
-		for(int i = 0; i < stack.size(); i++) {
+		for(int i = 0; stack.size() > 0; i++) {
 			target[i] = stack.pop();
 		}
+//		int i = 0;
+//		while(stack.size() > 0) {
+//			target[i] = stack.pop();
+//			i++;
+//		}
 		long end = System.currentTimeMillis();
 		System.out.println("用栈，耗时： " + (end - start));
 	}
@@ -47,15 +59,15 @@ public class ArrayReversal {
 	/**
 	 * 看stack中元素的顺序
 	 */
-//	public void arrayReversalByStack(int[] target) {
-//		Stack<Integer> stack = new Stack<>();
-//		for(Integer element : target) {
-//			stack.push(element);
-//		}
-//		for(int element : stack) {
-//			System.out.println(element);
-//		}
-//	}
+	public void printStackElement(int[] target) {
+		Stack<Integer> stack = new Stack<>();
+		for(Integer element : target) {
+			stack.push(element);
+		}
+		for(int element : stack) {
+			System.out.println(element);
+		}
+	}
 	
 	/**
 	 * 首尾交换
